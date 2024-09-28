dda242134e676bd41d63b91127fc4868df97dbee
Renan <renandiutida@gmail.com>
2024-09-28 20:44:15 -0300

    Estilizando e Organizando a página sobre

--- src/routes/Sobre/index.jsx
+++ src/routes/Sobre/index.jsx
@@ -1,11 +1,22 @@
 import { MainSobre } from "./sobre-styled"
+import SobreImagem from "./../../assets/sobre-background.png"
 
 
 const Sobre =()=>{
 
     return(
         <MainSobre>
-            <h1>Sobre</h1>
+            <h1>Sobre Nós</h1>
+            <section className='bem-vindo'>
+                <p>Bem-vindo à Eletric Life, onde a inovação em mobilidade sustentável se encontra com a excelência em atendimento ao cliente. Nossa missão é oferecer carros elétricos de alta qualidade a preços acessíveis, proporcionando uma experiência de compra satisfatória e segura para todos os nossos clientes. Estamos comprometidos em tornar a transição para veículos elétricos fácil e acessível, ajudando a construir um futuro mais verde e sustentável.</p>
+                <p>A equipe da Eletric Life é composta por profissionais apaixonados por mobilidade elétrica e inovação. Trabalhamos juntos para garantir que cada veículo oferecido atenda aos mais altos padrões de qualidade e eficiência, e que nossos clientes recebam um atendimento personalizado e atencioso. Nosso objetivo é criar uma jornada de compra que seja tão emocionante quanto dirigir um dos nossos veículos elétricos.</p>
+                <p>Além de oferecer veículos inovadores e ecologicamente responsáveis, estamos constantemente investindo em tecnologia e pesquisa para aprimorar nossa linha de produtos. A Eletric Life busca estar na vanguarda do mercado de carros elétricos, trazendo novidades e soluções que superem as expectativas dos nossos clientes. Cada modelo disponível em nossa loja foi selecionado cuidadosamente para garantir desempenho, segurança e a mais avançada tecnologia de mobilidade elétrica.</p>
+                <p>Acreditamos que a sustentabilidade vai além de nossos produtos. É por isso que estamos engajados em projetos de responsabilidade social e ambiental, apoiando iniciativas que buscam reduzir a pegada de carbono e promover o uso de energia limpa. Na Eletric Life, não estamos apenas vendendo carros; estamos impulsionando um movimento em direção a um futuro mais ecológico e consciente.</p>
+            </section>
+            <section className='obrigado'>
+                <p>Obrigado por escolher a Eletric Life. Estamos ansiosos para atendê-lo!</p>
+                <img src={SobreImagem} alt="Imagem Carro Elétrico" />
+            </section>
         </MainSobre>
     )
 }
--- src/routes/Sobre/sobre-styled.js
+++ src/routes/Sobre/sobre-styled.js
@@ -2,4 +2,51 @@ import styled from "styled-components";
 
 export const MainSobre = styled.main`
     margin-top: 90px;
+    padding: 40px 60px;
+
+    h1{
+        font-size: 2.8em;
+        font-weight: bold;
+        font-style: italic;
+        text-align: center;
+        margin-bottom: 30px;
+        color: #3C3CFF;
+    }
+
+    .bem-vindo{
+        display: flex;
+        flex-direction: column;
+        margin-top: 35px;
+    }
+
+    .bem-vindo p{
+        font-size: 1.3em;
+        text-indent: 65px;
+        text-align: justify;
+        margin-bottom: 10px;
+    }
+
+    .obrigado{
+        display: flex;
+        flex-direction: column;
+        align-items: center; /* Centraliza a imagem horizontalmente */
+        margin: 50px 0 30px 0;
+        text-align: center; /* Para centralizar o texto também */
+    }
+
+    .obrigado p{
+        font-size: 2em;
+        font-weight: bold;
+        font-style: italic;
+        color: #2424D4;
+        text-align: center;
+        margin-bottom: 40px;
+    }
+
+    .obrigado img{
+        max-width: 1000px;
+        width: 100%; /* Faz com que a imagem ocupe 100% da largura disponível */
+        height: auto; /* Mantém a proporção da imagem */
+        object-fit: contain; /* Garante que a imagem se ajuste bem ao espaço */
+    }
 `
